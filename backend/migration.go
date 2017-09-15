0e49d6cfd1e85a0ceafd88099a8bcbbb117125e6
sintell <a.anatskiy@hh.ru>
2017-09-15 19:23:03 +0300

    do not drop user&chars

--- backend/migration.go
+++ backend/migration.go
@@ -5,19 +5,16 @@ import "github.com/go-pg/pg/orm"
 func runMigrations() {
 	db := GetDB()
 
-	db.DropTable(&User{}, &orm.DropTableOptions{IfExists: true})
-	db.DropTable(&CharStat{}, &orm.DropTableOptions{IfExists: true})
 	db.DropTable(&Project{}, &orm.DropTableOptions{IfExists: true})
 	db.DropTable(&SkillValue{}, &orm.DropTableOptions{IfExists: true})
-	db.DropTable(&CharVar{}, &orm.DropTableOptions{IfExists: true})
 	db.DropTable(&ActiveEffect{}, &orm.DropTableOptions{IfExists: true})
 	db.DropTable(&Effect{}, &orm.DropTableOptions{IfExists: true})
 
-	db.CreateTable(&User{}, nil)
-	db.CreateTable(&CharStat{}, nil)
+	db.CreateTable(&User{}, &orm.CreateTableOptions{IfNotExists: true})
+	db.CreateTable(&CharStat{}, &orm.CreateTableOptions{IfNotExists: true})
+	db.CreateTable(&CharVar{}, &orm.CreateTableOptions{IfNotExists: true})
 	db.CreateTable(&Project{}, nil)
 	db.CreateTable(&SkillValue{}, nil)
-	db.CreateTable(&CharVar{}, nil)
 	db.CreateTable(&ActiveEffect{}, nil)
 	db.CreateTable(&Effect{}, nil)
 }
