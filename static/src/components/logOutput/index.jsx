eac94465cffb0a8d4d0d087f3c593bde3fb2626f
AndreyGladkov <andrewgladkovlz@gmail.com>
2017-09-15 21:32:38 +0300

    Add log output styles

--- static/src/components/logOutput/index.jsx
+++ static/src/components/logOutput/index.jsx
@@ -2,19 +2,26 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 
 const LogOutput = styled.div`
+        width: 100%;
         height: 200px;
         padding: 5px;
-        border: 4px solid red;
         font-size: 14px;
+        box-sizing: border-box;
+        color: #39ff00;
+        font-family: 'Ubuntu Mono', monospace;
 `;
 
 export default class LogOutputComponent extends Component {
     render() {
         return (
-            <LogOutput>
-                <p>1. Ударила молния</p>
-                <p>2. Приступил к работе с проектом блабла</p>
-            </LogOutput>
+            <div className='cube' style={{margin: '0 30px', background: '#2f2d2e'}}>
+                <div className='board'>
+                    <LogOutput>
+                        <p>1. Ударила молния</p>
+                        <p>2. Приступил к работе с проектом блабла</p>
+                    </LogOutput>
+                </div>
+            </div>
         );
     }
 }
