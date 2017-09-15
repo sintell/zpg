9bffd3139a640e26d6b0b432d07b91ccc1ea9480
Leonid Feskov <leonik89@gmail.com>
2017-09-15 20:50:25 +0300

    Design the board

--- static/src/components/kanbanBoard/index.jsx
+++ static/src/components/kanbanBoard/index.jsx
@@ -1,64 +1,60 @@
 import React, {Component} from 'react';
-import styled from 'styled-components';
 
-const KanbanBoard = styled.div`
-        display: flex;
-        border-top: 1px dotted black;
-        border-bottom: 1px dotted black;
-        border-rigth: 1px dotted black;
-        margin-bottom: 25px;
-`;
-
-const KanbanBoardCol = styled.div`
-        width: 20%;
-        padding: 10px;
-        border-left: 1px dotted black;
-        box-sizing: border-box;
-`;
-
-const KanbanBoardProject = styled.div`
-        width: 100%;
-        background: red;
-`;
 
 export default class KanbanBoardComponent extends Component {
-    renderProjectsTokanbanBoard(projects) {
+    renderProjectsTokanbanBoard(projects, color) {
         return projects.map((item) => {
             return (
-                <KanbanBoardProject key={item.id}>
-                    <p>{item.name}</p>
-                    <p>{item.desc}</p>
-                </KanbanBoardProject>
+                <div className={`sticker sticker_${color}`} key={item.id}>{item.name}</div>
             );
         });
     }
 
     render() {
         const {todoProjects, analyzeProjects, progProjects, testProjects, releasedProjects} = this.props;
+        const COLORS = {
+            todo: 'yellow',
+            analyze: 'red',
+            prog: 'blue',
+            test: 'green',
+            released: 'orange',
+        };
 
         return (
-            <KanbanBoard>
-                <KanbanBoardCol>
-                    <p>Todo</p>
-                    {this.renderProjectsTokanbanBoard(todoProjects)}
-                </KanbanBoardCol>
-                <KanbanBoardCol>
-                    <p>Аналитика</p>
-                    {this.renderProjectsTokanbanBoard(analyzeProjects)}
-                </KanbanBoardCol>
-                <KanbanBoardCol>
-                    <p>Программирование</p>
-                    {this.renderProjectsTokanbanBoard(progProjects)}
-                </KanbanBoardCol>
-                <KanbanBoardCol>
-                    <p>Тестирование</p>
-                    {this.renderProjectsTokanbanBoard(testProjects)}
-                </KanbanBoardCol>
-                <KanbanBoardCol>
-                    <p>Релиз</p>
-                    {this.renderProjectsTokanbanBoard(releasedProjects)}
-                </KanbanBoardCol>
-            </KanbanBoard>
+            <div className='cube'>
+                <div className='board'>
+                    <div className='board__col'>
+                        <div className='board__title board__title_todo'>Todo</div>
+                        <div className='board__content'>
+                            {this.renderProjectsTokanbanBoard(todoProjects, COLORS.todo)}
+                        </div>
+                    </div>
+                    <div className='board__col'>
+                        <div className='board__title board__title_analyze'>Analyze</div>
+                        <div className='board__content'>
+                            {this.renderProjectsTokanbanBoard(analyzeProjects, COLORS.analyze)}
+                        </div>
+                    </div>
+                    <div className='board__col'>
+                        <div className='board__title board__title_prog'>In progress</div>
+                        <div className='board__content'>
+                            {this.renderProjectsTokanbanBoard(progProjects, COLORS.prog)}
+                        </div>
+                    </div>
+                    <div className='board__col'>
+                        <div className='board__title board__title_test'>Testing</div>
+                        <div className='board__content'>
+                            {this.renderProjectsTokanbanBoard(testProjects, COLORS.test)}
+                        </div>
+                    </div>
+                    <div className='board__col'>
+                        <div className='board__title board__title_released'>Released</div>
+                        <div className='board__content'>
+                            {this.renderProjectsTokanbanBoard(releasedProjects, COLORS.released)}
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
