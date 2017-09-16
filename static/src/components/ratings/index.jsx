7d5347d7322ed1debc8b3bda66f7b07db5b02260
Leonid Feskov <leonik89@gmail.com>
2017-09-16 14:38:28 +0300

    Design rating

--- static/src/components/ratings/index.jsx
+++ static/src/components/ratings/index.jsx
@@ -8,12 +8,32 @@ class Ratings extends Component {
         this.props.fetchRating();
     }
 
-    renderItem(item) {
+    renderItem(item, index) {
         return (
-            <div key={item.id}>
-                <p>{item.name}</p>
-                <p>Уровень: {item.level}</p>
-            </div>
+            <tr key={item.id}>
+                <td className='rating-table__col-place'>{index + 1}</td>
+                <td width='60'>
+                    <span className={`entity__image entity__image_${(item.id % 3) + 1} entity__image_rating`} />
+                </td>
+                <td>{item.name}</td>
+                <td className='rating-table__col-level'>{item.level}</td>
+                <td>
+                    <div className='rating-skills'>
+                        <div className='rating-skill'>
+                            <span className='icon-stat icon-stat_analyze' /><br />
+                            1
+                        </div>
+                        <div className='rating-skill'>
+                            <span className='icon-stat icon-stat_prog' /><br />
+                            1
+                        </div>
+                        <div className='rating-skill'>
+                            <span className='icon-stat icon-stat_test' /><br />
+                            1
+                        </div>
+                    </div>
+                </td>
+            </tr>
         );
     }
 
@@ -21,8 +41,20 @@ class Ratings extends Component {
         const {items} = this.props;
 
         return (
-            <div>
-                {items.map(this.renderItem)}
+            <div className='rating-container'>
+                <table className='rating-table'>
+                    <thead>
+                        <tr>
+                            <th width='90'>Место</th>
+                            <th colSpan='2'>Персонаж</th>
+                            <th>Уровень</th>
+                            <th>Характеристики</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map(this.renderItem)}
+                    </tbody>
+                </table>
             </div>
         );
     }
