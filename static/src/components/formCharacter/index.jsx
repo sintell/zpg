3152162aff41b71cdf57d4874d5c89e2378aa555
Leonid Feskov <leonik89@gmail.com>
2017-09-16 17:28:54 +0300

    Fix

--- static/src/components/formCharacter/index.jsx
+++ static/src/components/formCharacter/index.jsx
@@ -44,8 +44,6 @@ class FormCharacter extends Component {
     }
 
     render() {
-        const companyName = 'Большие Тити';
-
         return (
             <form className='popup popup_char' onSubmit={this.submit}>
                 <div className='cube'>
@@ -63,8 +61,7 @@ class FormCharacter extends Component {
                                     onChange={this.changeInput}
                                     autoFocus />
                             </p>
-                            <p>Вы будете работаеть в компании «{companyName}»</p>
-                            <div className='separate'/>
+                            <div className='separate' />
                             <h2 className='h2'>Распределите очки характеристик:</h2>
                             <div className='params-list'>
                                 <div className='params-list__item'>
